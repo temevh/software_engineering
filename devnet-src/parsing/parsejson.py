6f913497395f297437e221af64ea9cbb0caf89ee
temevh <temehama@gmail.com>
2025-04-02 01:42:22 +0000

    lab 3.3.11 assignment changes

--- devnet-src/parsing/parsejson.py
+++ devnet-src/parsing/parsejson.py
@@ -10,6 +10,7 @@ with open('myfile_12250124.json', 'r') as json_file:
 #print("The access token is: {}".format(ourjson['access_token']))
 #print("The token expires in {} seconds.".format(ourjson['expires_in']))
 print("This was made by {}".format(ourjson['my_name']))
+print("This modification was added from the 12250124_branch as part of the other lag assignment")
 
 #print("\n\n---")
 #print(yaml.dump(ourjson))
