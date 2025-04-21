f900bde184166ece222856102842a868949b27cf
temevh <temehama@gmail.com>
2025-04-21 20:57:36 +0900

    midterm assignment init

---
+++ graphhopper/graphhopper_parse-json_1.py
@@ -0,0 +1,20 @@
+import requests
+import urllib.parse 
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+key = os.getenv("API_KEY")
+
+geocode_url = "https://graphhopper.com/api/1/geocode?"
+route_url = "https://graphhopper.com/api/1/route?"
+loc1 = "Washington, D.C."
+loc2 = "Baltimore, Maryland" 
+
+url = geocode_url + urllib.parse.urlencode({"q":loc1, "limit": "1", "key":key})
+
+replydata = requests.get(url)
+json_data = replydata.json()
+json_status = replydata.status_code
+print(json_data) 
