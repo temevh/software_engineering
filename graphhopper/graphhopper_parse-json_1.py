7c19c894a11da20c10c5723e3f6dc02940f6dcf5
temevh <temehama@gmail.com>
2025-04-21 21:07:31 +0900

    user input

--- graphhopper/graphhopper_parse-json_1.py
+++ graphhopper/graphhopper_parse-json_1.py
@@ -7,14 +7,54 @@ load_dotenv()
 
 key = os.getenv("API_KEY")
 
-geocode_url = "https://graphhopper.com/api/1/geocode?"
 route_url = "https://graphhopper.com/api/1/route?"
-loc1 = "Washington, D.C."
-loc2 = "Baltimore, Maryland" 
 
-url = geocode_url + urllib.parse.urlencode({"q":loc1, "limit": "1", "key":key})
+def geocoding (location, key):
+    geocode_url = "https://graphhopper.com/api/1/geocode?"
+    url = geocode_url + urllib.parse.urlencode({"q":location, "limit": "1",
+    "key":key})
+    replydata = requests.get(url)
+    json_data = replydata.json()
+    json_status = replydata.status_code
+    if json_status == 200:
+        lat = json_data["hits"][0]["point"]["lat"]
+        lng = json_data["hits"][0]["point"]["lng"]
+        name = json_data["hits"][0]["name"]
+        value = json_data["hits"][0]["osm_value"]
 
-replydata = requests.get(url)
-json_data = replydata.json()
-json_status = replydata.status_code
-print(json_data) 
+        if "country" in json_data["hits"][0]:
+            country = json_data["hits"][0]["country"]
+        else:
+            country=""
+
+        if "state" in json_data["hits"][0]:
+            state = json_data["hits"][0]["state"]
+        else:
+            state=""
+
+        if len(state) !=0 and len(country) !=0:
+            new_loc = name + ", " + state + ", " + country
+        elif len(state) !=0:
+            new_loc = name + ", " + country
+        else:
+            new_loc = name
+
+            print("Geocoding API URL for " + new_loc + " (Location Type: " + value + ")\n"
+            + url)
+    else:
+        lat="null"
+        lng="null"
+        new_loc=location
+    return json_status,lat,lng,new_loc 
+
+while True:
+    loc1 = input("Starting Location: ")
+    if (loc1 == "quit" or loc1 == "q"):
+        break
+    orig = geocoding(loc1, key)
+    print(orig)
+    loc2 = input("Destination: ")
+    if (loc2 == "quit" or loc2 == "q"):
+        break
+    dest = geocoding(loc2, key)
+    print(dest)
