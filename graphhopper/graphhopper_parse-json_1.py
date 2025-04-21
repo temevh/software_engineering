c0cc7f432c008a7e20b480ab0aa45d3444a0c18b
temevh <temehama@gmail.com>
2025-04-21 21:56:46 +0900

    fixed error

--- graphhopper/graphhopper_parse-json_1.py
+++ graphhopper/graphhopper_parse-json_1.py
@@ -10,9 +10,12 @@ key = os.getenv("API_KEY")
 route_url = "https://graphhopper.com/api/1/route?"
 
 def geocoding (location, key):
+    while location == "":
+        location = input("Enter the location again: ")
     geocode_url = "https://graphhopper.com/api/1/geocode?"
     url = geocode_url + urllib.parse.urlencode({"q":location, "limit": "1",
     "key":key})
+    print(url)
     replydata = requests.get(url)
     json_data = replydata.json()
     json_status = replydata.status_code
@@ -45,6 +48,8 @@ def geocoding (location, key):
         lat="null"
         lng="null"
         new_loc=location
+        if json_status != 200:
+            print("Geocode API status: " + str(json_status) + "\nError message: " + json_data["message"]) 
     return json_status,lat,lng,new_loc 
 
 while True:
@@ -57,4 +62,13 @@ while True:
     if (loc2 == "quit" or loc2 == "q"):
         break
     dest = geocoding(loc2, key)
+    print("=================================================")
+    if orig[0] == 200 and dest[0] == 200:
+        op="&point="+str(orig[1])+"%2C"+str(orig[2])
+        dp="&point="+str(dest[1])+"%2C"+str(dest[2])
+        paths_url = route_url + urllib.parse.urlencode({"key":key}) + op + dp
+        paths_status = requests.get(paths_url).status_code
+        paths_data = requests.get(paths_url).json()
+        print("Routing API Status: " + str(paths_status) + "\nRouting API URL:\n" +
+        paths_url)
     print(dest)
